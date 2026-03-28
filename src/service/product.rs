8ce465d8f68e59c725bc3e7f292ffa6804e6f823
moondiverc <nezzaluna10@gmail.com>
2026-03-29 00:00:42 +0700

    Edit Product service methods to call notify after create/delete.

--- src/service/product.rs
+++ src/service/product.rs
@@ -1,5 +1,6 @@
 use rocket::http::Status;
 use rocket::serde::json::Json;
+use crate::service::notification::NotificationService;
 
 use bambangshop::{Result, compose_error_response};
 use crate::model::product::Product;
@@ -12,6 +13,8 @@ impl ProductService {
         product.product_type = product.product_type.to_uppercase();
         let product_result: Product = ProductRepository::add(product);
 
+        // ::/.
+        NotificationService.notify(&product_result.product_type, "CREATED", product_result.clone());
         return Ok(product_result);
     }
 
@@ -40,6 +43,7 @@ impl ProductService {
         }
         let product: Product = product_opt.unwrap();
 
+        NotificationService.notify(&product.product_type, "DELETED", product.clone());
         return Ok(Json::from(product));
     }
 
